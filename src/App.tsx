550ded3873af51b0b25d6875d4d3ca91af8c6d12
2njeong <twilight9758@gmail.com>
2025-01-07 00:54:04 +0900

    #3_[feat] - Hello, world 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import VoteOptions from "./components/create/VoteOptions";
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello, world!</h1>
       <VoteOptions />
     </div>
   );
