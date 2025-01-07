68b74e82ef61a416bc2876aefd7dd42e0508214a
Seojin Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2025-01-08 00:42:48 +0900

    #1 feat: 투표 생성 화면 UI 구현 (#8)

---
+++ src/components/create/createForm.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+import { useForm, FormProvider } from "react-hook-form";
+import styled from "styled-components";
+
+import { BasicForm } from "./steps/BasicForm";
+import { LandingForm } from "./steps/LandingForm";
+import { Button } from "../common/Button";
+
+export const CreateForm = () => {
+  const methods = useForm();
+  const [step, setStep] = useState<number>(1);
+
+  return (
+    <FormProvider {...methods}>
+      <form>
+        {step === 1 && <LandingForm />}
+        {step === 2 && <BasicForm />}
+        <ButtonContainer>
+          {step === 1 && (
+            <Button type="button" onClick={() => setStep(step + 1)}>
+              투표 생성하기
+            </Button>
+          )}
+          {step > 1 && step < 4 && (
+            <Button
+              type="button"
+              variant="secondary"
+              onClick={() => setStep(step - 1)}
+              style={{ alignSelf: "flex-start" }}
+            >
+              이전
+            </Button>
+          )}
+          {step === 2 && (
+            <Button type="button" variant="primary" onClick={() => setStep(step + 1)} style={{ marginLeft: "auto" }}>
+              다음
+            </Button>
+          )}
+          {step === 3 && (
+            <Button
+              type="button"
+              variant="primary"
+              onClick={methods.handleSubmit(() => {
+                console.log("생성 완료");
+              })}
+              style={{ marginLeft: "auto" }}
+            >
+              생성하기
+            </Button>
+          )}
+        </ButtonContainer>
+      </form>
+    </FormProvider>
+  );
+};
+
+const ButtonContainer = styled.div<{ center?: boolean }>`
+  position: fixed;
+  bottom: 20px;
+  width: 100%;
+  display: flex;
+  justify-content: ${({ center }) => (center ? "center" : "space-between")};
+  padding: 0 20px;
+  box-sizing: border-box;
+`;
---
+++ src/components/create/steps/BasicForm.tsx
@@ -0,0 +1,50 @@
+import { useFormContext, Controller } from "react-hook-form";
+
+import { Vote } from "../../../types/voteTypes";
+import { Input } from "../../common/Input";
+import { Select } from "../../common/Select";
+
+export const BasicForm = () => {
+  const { control } = useFormContext<Vote>();
+
+  return (
+    <div>
+      <Controller
+        name="title"
+        control={control}
+        defaultValue=""
+        rules={{ required: "투표 제목 입력이 필요합니다" }}
+        render={({ field, fieldState: { error } }) => (
+          <Input label="제목" placeholder="제목" {...field} error={error?.message} />
+        )}
+      />
+      <Controller
+        name="nickname"
+        control={control}
+        defaultValue=""
+        rules={{ required: "방장 닉네임 입력이 필요합니다" }}
+        render={({ field, fieldState: { error } }) => (
+          <Input label="닉네임" placeholder="닉네임" {...field} error={error?.message} />
+        )}
+      />
+      <Controller
+        name="options"
+        control={control}
+        defaultValue="majority"
+        rules={{ required: "투표 옵션 선택이 필요합니다" }}
+        render={({ field, fieldState: { error } }) => (
+          <Select
+            label="옵션"
+            options={[
+              { value: "majority", label: "다수결" },
+              { value: "random", label: "랜덤" },
+              { value: "lottery", label: "제비뽑기" },
+            ]}
+            {...field}
+            error={error?.message}
+          />
+        )}
+      />
+    </div>
+  );
+};
