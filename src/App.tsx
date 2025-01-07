2833e9617e58d876bc4f7b7f355edd57d7c68b2c
2njeong <twilight9758@gmail.com>
2025-01-07 23:13:25 +0900

    #2_[feat] - 공유링크 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import CreateNShareVote from "./components/create/CreateNShareVote";
 import VoteOptions from "./components/create/VoteOptions";
 
 const App: React.FC = () => {
   return (
     <div>
       <VoteOptions />
+      <CreateNShareVote />
     </div>
   );
 };
--- src/components/create/CreateNShareVote.tsx
+++ src/components/create/CreateNShareVote.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
 const CreateNShareVote = () => {
+  const [randomLink, setRandomLink] = useState("");
+  const navigate = useNavigate();
+
+  const generateLink = () => {
+    const randomId = uuid();
+    const link = `/vote/${randomId}`;
+    setRandomLink(link);
+    return link;
+  };
+
+  const handleNavigate = () => {
+    const link = generateLink();
+    navigate(link);
+  };
+
+  const handleCopy = () => {
+    const fullUrl = `${window.location.origin}${randomLink}`;
+    navigator.clipboard
+      .writeText(fullUrl)
+      .then(() => alert("링크가 클립보드에 복사되었습니다!"))
+      .catch((err) => alert(`복사 실패! ${err}`));
+  };
+
   return (
     <div>
       <h2 style={{ whiteSpace: "pre-line" }}>투표가 완료되었습니다. {"\n"}투표를 공유해보세요!</h2>
-      <button>투표화면으로 이동하기</button>
-      <button>공유하기</button>
+      <button onClick={handleNavigate}>투표화면으로 이동하기</button>
+      <button onClick={handleCopy}>공유하기</button>
     </div>
   );
 };
