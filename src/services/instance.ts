f4f89e5d1eff1a92e1ae66cad18506690d78fdef
SJ-Kwak <seojin7@ewhain.net>
2025-01-23 23:50:23 +0900

    #19 [feat] - axios instance, interceptor 생성

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -1,1 +1,27 @@
-console.log(process.env.API_URL);
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.API_URL,
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config;
+  },
+  (error: unknown) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  async (response: AxiosResponse) => {
+    return response.data;
+  },
+  async (error) => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
