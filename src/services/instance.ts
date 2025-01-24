c147ceb45d49c24b9949608105cd8953ea4ada9c
Seojin Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2025-01-24 14:34:15 +0900

    #19 feat: axios 연결을 위한 request 모듈 구현 (#25)
    * #19 [feat] - env 설정
    
    * #19 [feat] - axios instance, interceptor 생성
    
    * #19 [feat] - request 모듈 생성

---
+++ src/services/instance.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.API_URL,
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config;
+  },
+  (error: unknown) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  async (response: AxiosResponse) => {
+    return response.data;
+  },
+  async (error) => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
