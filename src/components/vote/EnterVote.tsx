7e8d587a75def03b97dfc0b81b2d01aaf4c9208b
Seojin Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2025-02-01 13:09:51 +0900

    #37 feat: 투표 입장 UI 구현 (#39)
    * #37 [feat] - EnterVote 생성
    
    * #37 [feat] - 투표 입장 UI 구현
    
    * Delete src/components/vote/voteForm.tsx

---
+++ src/components/vote/EnterVote.tsx
@@ -0,0 +1,54 @@
+import { Dispatch, SetStateAction } from "react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
+import { Button } from "../common";
+import LogoWithInput from "./LogoWithInput";
+import useWebsocketUrl from "@/hooks/useWebsocketUrl";
+
+interface EnterVoteProps {
+  setStep: Dispatch<SetStateAction<number>>;
+}
+
+const EnterVote = ({ setStep }: EnterVoteProps) => {
+  const { websocketUrl } = useWebsocketUrl();
+  console.log(websocketUrl);
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async () => {
+    console.log("submit");
+    setStep(2);
+  };
+
+  return (
+    <Wrapper onSubmit={handleSubmit(onSubmit)}>
+      <Title>투표 제목</Title>
+
+      <Button type="submit" style={{ position: "absolute", bottom: 20, zIndex: 100 }}>
+        투표 입장하기
+      </Button>
+
+      <LogoWithInput register={register} />
+    </Wrapper>
+  );
+};
+
+export default EnterVote;
+
+const Wrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  height: 20vh;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- src/pages/VotePage.tsx
+++ src/pages/VotePage.tsx
@@ -1,22 +1,18 @@
-import { useParams } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { modalState } from "@/atoms/voteAtom";
-import useSessionId from "@/hooks/useSessionId";
-import useWebsocketUrl from "@/hooks/useWebsocketUrl";
+import VoteForm from "@/components/vote/VoteForm";
 
 const VotePage = () => {
-  const { id } = useParams();
-  const [{ isOpen }, setModalState] = useRecoilState(modalState);
-  const { websocketUrl } = useWebsocketUrl();
-  const { sessionId } = useSessionId();
-
-  console.log("websocket", websocketUrl, "sessionid", sessionId);
-
-  console.log("isOpen =>", isOpen);
   return (
-    <div>
-      <h2>투표 id: {id}</h2>
-      <button onClick={() => setModalState((prev) => ({ ...prev, isOpen: true }))}>테스트</button>
+    <div
+      style={{
+        height: "100vh",
+        overflow: "hidden",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <VoteForm />
     </div>
   );
 };
