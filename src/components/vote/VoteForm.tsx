18cce22d2841179db5c307e94d3d1000cbeeeb94
SJ-Kwak <seojin7@ewhain.net>
2025-03-24 23:28:03 +0900

    #48 [fix] - 투표 참여자도 종료 시, 결과 화면으로 이동하도록 수정

--- src/components/vote/VoteForm.tsx
+++ src/components/vote/VoteForm.tsx
@@ -21,11 +21,12 @@ const VoteForm = () => {
           clearInterval(intervalId); // 한 번 실행된 후 더 이상 체크할 필요 없으므로 정리
           navigate(`/votes/${voteUuid}/results`);
         }
+        console.log("check vote expired");
       }, 1000); // 1초마다 실행
 
       return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 정리
     }
-  }, []);
+  }, [voteUuid]);
 
   const steps: { [key: number]: JSX.Element } = {
     1: <EnterVote />,
